efa9830e29f9d17aa63c908c8237292c9f6e5beb
lushuangyu <lushuangyu027043@gtjas.com>
2025-03-31 17:35:21 +0800

    feat(README): readme

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { IRequestData } from './types'
-const API_KEY = '4bbd1e648ee0483a9c661708253103' // open'12d2891a7ccbc04b80d5c667c1b86fd0' // 替换为你自己的 API Key
-const BASE_URL = 'http://api.weatherapi.com/v1' // 'https://api.openweathermap.org/data/2.5'
+const API_KEY = '4bbd1e648ee0483a9c661708253103' // api key 注册weatherapi 获取
+const BASE_URL = 'http://api.weatherapi.com/v1' // 接口路径
 
 export const getWeather = async (city: string): Promise<IRequestData> => {
   try {
